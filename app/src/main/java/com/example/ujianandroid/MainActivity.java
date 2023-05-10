3286bdcf76e771101b97e72802c5475cbfdbe844
mey <meytriwidya34@gmail.com>
2023-05-10 20:05:46 +0700

    UTS mobile programing

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -33,18 +32,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
-                String isian_umur = edUmur.getText().toString();
+                Integer isian_umur = Integer.valueOf(edUmur.getText().toString());
 
-                if(isian_nama_depan.isEmpty()){
+                if(isian_nama_depan.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
                     daftar_nama.clear();
 
                     for (int i = 0; i < isian_umur; i++) {
                         daftar_nama.add(nama_lengkap);
-
-                }
+                    }
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
